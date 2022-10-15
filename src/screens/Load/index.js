886e4a4fadb998ae22bd2c36efab8b785dfd1188
dhiyo7 <dhiyoreksa12@gmail.com>
2022-10-16 04:45:05 +0700

    all screen are covered

---
+++ src/screens/Load/index.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import {StatusBar, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../../config';
+import {getLocalStorage} from '../../config/Storage';
+import {NobiLogo} from '../../config/Svgs';
+import * as NavigationService from '../../navigation/NavigationApp';
+import Styles from './styles';
+
+export default function Load() {
+  useEffect(() => {
+    const dataLocal = () => {
+      setTimeout(async () => {
+        const data = await getLocalStorage();
+        if (data !== null && data.logIn === 'isLogIn') {
+          NavigationService.mainTab();
+        } else {
+          NavigationService.resetStack('Login');
+        }
+      }, 3000);
+    };
+    dataLocal();
+  }, []);
+  return (
+    <>
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      <LinearGradient
+        style={Styles.container}
+        colors={[
+          Colors.main.baseBackground1,
+          Colors.main.baseBackground2,
+          Colors.main.baseBackground2,
+        ]}>
+        <View style={Styles.mainLogo}>
+          <NobiLogo height={100} width={100} />
+        </View>
+      </LinearGradient>
+    </>
+  );
+}
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,35 @@
+import {Colors} from '../../config';
+
+export default {
+  header: {
+    container: {
+      alignSelf: 'center',
+    },
+  },
+  bottomDisable: {
+    innerContainer: {
+      backgroundColor: Colors.main.baseWhite15,
+      marginHorizontal: 47,
+      marginBottom: 49,
+    },
+    label: {
+      color: Colors.main.baseWhite,
+    },
+  },
+  bottomEnable: {
+    innerContainer: {
+      backgroundColor: Colors.main.baseColor,
+      marginHorizontal: 47,
+      marginBottom: 49,
+    },
+    label: {
+      color: Colors.main.baseWhite,
+    },
+  },
+  form: {
+    container: {
+      marginHorizontal: 24,
+      marginTop: 44,
+    },
+  },
+};
